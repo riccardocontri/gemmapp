2763c1b5c81e79b20d716858c395591052b03f13
Riccardo Contri <riccardocontri@libero.it>
2011-11-03 11:41:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemmapp::Application.config.secret_token = 'c3e45df2e02d86d8c56537977d195b316633c861f57f410b2c8f345ffc7602c11921c3f9bcc4eed468dae7f9d71aa75037558f344d5eb59b10dddbb4576d0428'
