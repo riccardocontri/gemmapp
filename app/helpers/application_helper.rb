e0a19916818a764585b0bbefa1c58091070ab6b8
Riccardo Contri <riccardocontri@libero.it>
2011-11-22 17:40:31 +0100

    Extracted logo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     def page_title
         APP_NAME + ((@page_name.blank?) ? "" : " | #{@page_name}")
     end
+
+    def logo
+        image_tag("logo.png", :size => "76x67", :alt => APP_NAME, :class => "round")
+    end
 end
