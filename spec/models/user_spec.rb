db4b5bcf26921f85db85f7ea05dacc1291287054
Riccardo Contri <riccardocontri@libero.it>
2011-11-23 01:01:22 +0100

    Created user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  surname    :string(255)
+#  nickname   :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'spec_helper'
+
+describe User do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
