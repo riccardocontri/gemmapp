085b0399fbc54f47492bdc1537ec58622b86c62b
Riccardo Contri <riccardocontri@libero.it>
2011-12-02 02:38:05 +0100

    Added user password management with insecure encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,17 @@
 
 EMAIL_VALIDATION_TEMPLATE = /^[\w.+-]+@[a-z\d.-]+\.[a-z]{2,4}$/i
 MAXIMUM_LENGTH_FOR_NAMES = 64
+MINIMUM_LENGTH_FOR_PASSWORD = 8
+MAXIMUM_LENGTH_FOR_PASSWORD = 255
 
 class User < ActiveRecord::Base
-    attr_accessible :name, :surname, :nickname, :email
+    attr_accessor :password
+    attr_accessible :name,
+                    :surname,
+                    :nickname,
+                    :email,
+                    :password,
+                    :password_confirmation
 
     validates :name, :presence => true,
                      :length =>  { :maximum => MAXIMUM_LENGTH_FOR_NAMES }
@@ -27,4 +35,25 @@ class User < ActiveRecord::Base
     validates :email, :presence => true,
                       :format => { :with => EMAIL_VALIDATION_TEMPLATE },
                       :uniqueness => { :case_sensitive => false }
+
+    validates :password, :presence => true,
+                         :confirmation => true,
+                         :length =>  { :within =>
+                            MINIMUM_LENGTH_FOR_PASSWORD..MAXIMUM_LENGTH_FOR_PASSWORD}
+
+    before_save :encrypt_password
+
+    # Checks if the submitted password matches the stored password.
+    def has_password?(submitted_password)
+        encrypted_password == encrypt(submitted_password)
+    end
+
+    private
+        def encrypt_password
+            self.encrypted_password = encrypt(password)
+        end
+
+        def encrypt(text)
+            text.reverse #TODO Replace with real encryption
+        end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,9 @@ require 'spec_helper'
 describe User do
     before(:all) do
         @max_allowed_lenth_for_names = 64
+        @min_allowed_lenth_for_passwords = 8
+        @max_allowed_lenth_for_passwords = 255
+        @valid_password = '123Password'
     end
 
     before(:each) do
@@ -24,60 +27,138 @@ describe User do
             :name => "user name",
             :surname => "user surname",
             :nickname => "user nickname",
-            :email => "username@domain.com"
+            :email => "username@domain.com",
+            :password => @valid_password,
+            :password_confirmation => @valid_password
         }
         @user_attrs = valid_user_attrs
     end
 
-    it "Should reject a blank name" do
+    it "should reject a blank name" do
         user = User.new(@user_attrs.merge(:name => ""))
         user.should_not be_valid
     end
 
-    it "Should accept a blank surname" do
+    it "should accept a blank surname" do
         user = User.new(@user_attrs.merge(:surname => ""))
         user.should be_valid
     end
 
-    it "Should accept a blank nickname" do
+    it "should accept a blank nickname" do
         user = User.new(@user_attrs.merge(:nickname => ""))
         user.should be_valid
     end
 
-    it "Should reject a name that is too long" do
+    it "should reject a name that is too long" do
         valid_name = "V" * @max_allowed_lenth_for_names
         invalid_name = "I" * (@max_allowed_lenth_for_names + 1)
 
         valid_user = User.new(@user_attrs.merge(:name => valid_name))
-        valid_user.should be_valid, "A name of #{@max_allowed_lenth_for_names} characters should be accepted"
+        valid_user.should be_valid, "A name of #{valid_name.length} characters should be accepted"
 
         invalid_user = User.new(@user_attrs.merge(:name => invalid_name))
-        invalid_user.should_not be_valid, "A name of #{@max_allowed_lenth_for_names + 1} characters should be rejected"
+        invalid_user.should_not be_valid, "A name of #{invalid_name.length} characters should be rejected"
     end
 
-    it "Should reject a surname that is too long" do
+    it "should reject a surname that is too long" do
         valid_surname = "V" * @max_allowed_lenth_for_names
         invalid_surname = "I" * (@max_allowed_lenth_for_names + 1)
 
         valid_user = User.new(@user_attrs.merge(:surname => valid_surname))
-        valid_user.should be_valid, "A surname of #{@max_allowed_lenth_for_names} characters should be accepted"
+        valid_user.should be_valid, "A surname of #{valid_surname.length} characters should be accepted"
 
         invalid_user = User.new(@user_attrs.merge(:surname => invalid_surname))
-        invalid_user.should_not be_valid, "A surname of #{@max_allowed_lenth_for_names + 1} characters should be rejected"
+        invalid_user.should_not be_valid, "A surname of #{invalid_surname.length} characters should be rejected"
     end
 
-    it "Should reject a nickname that is too long" do
+    it "should reject a nickname that is too long" do
         valid_nickname = "V" * @max_allowed_lenth_for_names
         invalid_nickname = "I" * (@max_allowed_lenth_for_names + 1)
 
         valid_user = User.new(@user_attrs.merge(:nickname => valid_nickname))
-        valid_user.should be_valid, "A nickname of #{@max_allowed_lenth_for_names} characters should be accepted"
+        valid_user.should be_valid, "A nickname of #{valid_nickname.length} characters should be accepted"
 
         invalid_user = User.new(@user_attrs.merge(:nickname => invalid_nickname))
-        invalid_user.should_not be_valid, "A nickname of #{@max_allowed_lenth_for_names + 1} characters should be rejected"
+        invalid_user.should_not be_valid, "A nickname of #{invalid_nickname.length} characters should be rejected"
     end
 
-    it "Should accept valid emails" do
+    it "should reject a blank password" do
+        attrs = @user_attrs.merge(:password => "", :password_confirmation => "")
+        user = User.new(attrs)
+        user.should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+        attrs = @user_attrs.merge(:password => "123ABCdef", :password_confirmation => "fedCBA321")
+        user = User.new(attrs)
+        user.should_not be_valid
+    end
+
+    it "should reject a password that is too short" do
+        valid_password = @valid_password[0, @min_allowed_lenth_for_passwords]
+        invalid_password = valid_password.chop
+
+        valid_attrs = @user_attrs.merge(
+            :password => valid_password,
+            :password_confirmation => valid_password)
+        valid_user = User.new(valid_attrs)
+        valid_user.should be_valid,
+            "A password of #{valid_password.length} characters should be accepted"
+
+        invalid_attrs = @user_attrs.merge(
+            :password => invalid_password,
+            :password_confirmation => invalid_password)
+        invalid_user = User.new(invalid_attrs)
+        invalid_user.should_not be_valid,
+            "A password of #{invalid_password.length} characters should be rejected"
+    end
+
+    it "should reject a password that is too long" do
+        valid_password = (@valid_password * 40)[0, @max_allowed_lenth_for_passwords]
+        invalid_password = valid_password + '+'
+
+        valid_attrs = @user_attrs.merge(
+            :password => valid_password,
+            :password_confirmation => valid_password)
+        valid_user = User.new(valid_attrs)
+        valid_user.should be_valid,
+            "A password of #{valid_password.length} characters should be accepted"
+
+        invalid_attrs = @user_attrs.merge(
+            :password => invalid_password,
+            :password_confirmation => invalid_password)
+        invalid_user = User.new(invalid_attrs)
+        invalid_user.should_not be_valid,
+            "A password of #{invalid_password.length} characters should be rejected"
+    end
+
+    describe "password persistence" do
+        before(:each) do
+            @user = User.create!(@user_attrs)
+        end
+
+        it "should save an encrypted password" do
+            @user.encrypted_password.should_not be_blank
+        end
+
+        describe "password checking" do
+            it "should return true if submitted password matches stored one" do
+                #~ password = "1234Abcd"
+                #~ attrs = @user_attrs.merge(
+                    #~ :password => password,
+                    #~ :password_confirmation => password)
+                #~ @user = User.create!(attrs)
+
+                @user.has_password?(@user_attrs[:password]).should be_true
+            end
+
+            it "should return false if submitted password doesn't match stored one" do
+                @user.has_password?(@user_attrs[:password].reverse).should be_false
+            end
+        end
+    end
+
+    it "should accept valid emails" do
         valid_emails =
         [
             "username@domain.org",
@@ -93,7 +174,7 @@ describe User do
         end
     end
 
-    it "Should reject invalid emails" do
+    it "should reject invalid emails" do
         invalid_emails =
         [
             "",
@@ -109,25 +190,25 @@ describe User do
         end
     end
 
-    it "Should have unique email" do
+    it "should have unique email" do
         same_email = "user@domain.com"
 
-        first_user = User.new(:name => "First", :email => same_email)
+        first_user = User.new(@user_attrs.merge(:name => "First", :email => same_email))
         first_user.should be_valid
         first_user.save
 
-        second_user = User.new(:name => "Second", :email => same_email)
+        second_user = User.new(@user_attrs.merge(:name => "Second", :email => same_email))
         second_user.should_not be_valid
     end
 
-    it "Should reject emails only differing by case" do
+    it "should reject emails only differing by case" do
         same_email = "user@domain.com"
 
-        first_user = User.new(:name => "First", :email => same_email.downcase)
+        first_user = User.new(@user_attrs.merge(:name => "First", :email => same_email.downcase))
         first_user.should be_valid
         first_user.save
 
-        second_user = User.new(:name => "Second", :email => same_email.upcase)
+        second_user = User.new(@user_attrs.merge(:name => "Second", :email => same_email.upcase))
         second_user.should_not be_valid
     end
 end
