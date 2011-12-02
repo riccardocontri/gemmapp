e8bc1c6e626962f9c735f3c75d62860038c06ae2
Riccardo Contri <riccardocontri@libero.it>
2011-12-02 01:11:59 +0100

    Created user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  def new
-    @page_name = 'Join'
-  end
+    def new
+        @page_name = 'Join'
+    end
+
+    def show
+        #TODO @title = ...
+        @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Gemmapp::Application.routes.draw do
   #get "users/new"
+  resources :users
   match '/join', :to => 'users#new'
 
   # get "pages/home"
