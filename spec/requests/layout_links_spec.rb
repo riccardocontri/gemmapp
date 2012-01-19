8d78cf87bd2822b80fb1de560bc570a19efdd372
Riccardo Contri <riccardocontri@libero.it>
2012-01-19 20:39:03 +0100

    Added layout links to sign in/out

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,40 +2,89 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
-  it "Should have valid links" do
-    visit root_path
+    it "Should have valid links" do
+        visit root_path
 
-    click_link "about"
-    response.should have_selector("title", :content => "About")
+        click_link "about"
+        response.should have_selector("title", :content => "About")
 
-    click_link "contatti"
-    response.should have_selector("title", :content => "Contacts")
+        click_link "contatti"
+        response.should have_selector("title", :content => "Contacts")
 
-    click_link "Home"
-    response.should have_selector("title", :content => "Home")
-  end
-
-  describe "GET '/'" do
-    it "Should open the Home page" do
-      get "/"
-      response.should have_selector("title", :content => "Home")
+        click_link "Home"
+        response.should have_selector("title", :content => "Home")
     end
-  end
 
-  describe "GET 'about'" do
-    it "Should open the About page" do
-      get "/about"
-      response.should have_selector("title", :content => "About")
+    describe "GET '/'" do
+        it "Should open the Home page" do
+            get "/"
+            response.should have_selector("title", :content => "Home")
+        end
     end
-  end
 
-  describe "GET 'contacts'" do
-    it "Should open the Contacts page" do
-      get "/contacts"
-      response.should have_selector("title", :content => "Contacts")
+    describe "GET 'about'" do
+        it "Should open the About page" do
+            get "/about"
+            response.should have_selector("title", :content => "About")
+        end
     end
-  end
 
+    describe "GET 'contacts'" do
+        it "Should open the Contacts page" do
+            get "/contacts"
+            response.should have_selector("title", :content => "Contacts")
+        end
+    end
+
+    describe "when not signed in" do
+        before(:each) do
+            visit root_path
+            controller.sign_out
+        end
+
+        it "should have a 'signin' link" do
+            response.should have_selector("header") do |h|
+                h.should have_selector("a", :href => signin_path)
+            end
+        end
+
+        it "should not have a 'signout' link" do
+            response.should have_selector("header") do |h|
+                h.should_not have_selector("a", :href => signout_path)
+            end
+        end
+    end
+
+    describe "when signed in" do
+        before(:each) do
+            @user = Factory(:user)
+            visit signin_path
+            fill_in :session_email, :with => @user.email
+            fill_in :session_password, :with => @user.password
+            click_button
+        end
+
+        it "should have a 'signout' link" do
+            response.should have_selector("header") do |h|
+                h.should have_selector("a", :href => signout_path)
+            end
+        end
+
+        it "should not have a 'signin' link" do
+            response.should have_selector("header") do |h|
+                h.should_not have_selector("a", :href => signin_path)
+            end
+        end
+
+        it "should have a 'profile' link" do
+            response.should have_selector("header") do |h|
+                h.should have_selector("a", :href => user_path(@user))
+            end
+        end
+    end
+
+
+#TODO Spostare altrove (non c'entrano con il layout)
   describe "GET 'join'" do
     it "Should open the New user page" do
       get "/join"
@@ -43,13 +92,4 @@ describe "LayoutLinks" do
       response.should have_selector("title", :content => "Join")
     end
   end
-
-  pending "GET 'signin'" do
-    it "Should open the new session page" do
-      get "/signin"
-      response.should render_template("sessions/new")
-      response.should have_selector("title", :content => "Sign In") #TODO ??? Già in session_controllers_spec
-    end
-  end
-
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
 
-describe "Sign in" do
+describe "Sign in management" do
     describe "with invalid attributes" do
-        pending "should fail" do
+        it "should not open an user session" do
             visit signin_path
-            fill_in :session_email, :with => "" #"n@domain.com"
-            fill_in :session_password, :with => "" #"123Password"
+            fill_in :session_email, :with => ""
+            fill_in :session_password, :with => ""
             click_button
-            response.should render_template("users/new")
-            response.should have_selector("div#error_explanation")
+            response.should have_selector("div.flash.error")
+            controller.current_user.should be_nil
+        end
+    end
+
+    describe "with valid attributes" do
+        it "should allow the user to sign in and out" do
+            user = Factory(:user)
+            visit signin_path
+            fill_in :session_email, :with => user.email
+            fill_in :session_password, :with => user.password
+            click_button
+            controller.current_user.should == user
+            click_link 'signout_link'
+            controller.current_user.should be_nil
         end
     end
 end
