603a3cb6ff0e293fbf1c7ee09937ed08ab5bd49d
Riccardo Contri <riccardocontri@libero.it>
2012-02-07 17:17:17 +0100

    (Refactored sessions_spec)

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe "Sign in management" do
+    def sign_in(user)
+        visit signin_path
+        fill_in :session_email, :with => user.email
+        fill_in :session_password, :with => user.password
+        click_button
+    end
+
     describe "with invalid attributes" do
         it "should not open an user session" do
-            visit signin_path
-            fill_in :session_email, :with => ""
-            fill_in :session_password, :with => ""
-            click_button
+            invalid_user = User.new
+            sign_in(invalid_user)
+
             response.should have_selector("div.flash.error")
             controller.current_user.should be_nil
         end
@@ -14,12 +20,10 @@ describe "Sign in management" do
 
     describe "with valid attributes" do
         it "should allow the user to sign in and out" do
-            user = Factory(:user)
-            visit signin_path
-            fill_in :session_email, :with => user.email
-            fill_in :session_password, :with => user.password
-            click_button
-            controller.current_user.should == user
+            valid_user = Factory(:user)
+            sign_in(valid_user)
+
+            controller.current_user.should == valid_user
             click_link 'signout_link'
             controller.current_user.should be_nil
         end
